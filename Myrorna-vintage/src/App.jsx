a0657e9a206e9509662395e4cf9d31a40cf309ba
samuelindberg <samuel.lindberg01@icloud.com>
2022-12-12 18:58:55 +0100

    nav påbörjad

--- Myrorna-vintage/src/App.jsx
+++ Myrorna-vintage/src/App.jsx
@@ -1,10 +1,9 @@
-
 import './App.css'
-
+import Header from './Global_components/Header'
 function App() {
-
   return (
     <div className="App">
+      <Header />
     </div>
   )
 }
---
+++ Myrorna-vintage/src/Global_components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <h1 id="logo">Myrorna Vintage</h1>
+      <nav>
+        <a id='' href="">Home</a>
+        <a id='' href="">New arrivals</a>
+        <a id='' href="">Hand Picked</a>
+        <a id='' href="">Men</a>
+        <a id='' href="">Women</a>
+      </nav>
+    </header>
+  )
+}
