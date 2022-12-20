07499db0b1aa02941be041d555f51a0187d61bd7
samuelindberg <samuel.lindberg01@icloud.com>
2022-12-20 18:08:02 +0100

    databas kopplad, handpicked items visas (4 st, bestämt i databasen

--- Myrorna-vintage/src/intro-page/Handpicked.jsx
+++ Myrorna-vintage/src/intro-page/Handpicked.jsx
@@ -1,47 +1,28 @@
-import React,{useState,useEffect} from 'react'
-import jacket from '../assets/pilotjacket.png'
-import bluehat from '../assets/bluehat.png'
-import HandpickedCard from './HandpickedCard'
-import Datafetching from './Datafetching'
-const pilotJacket = {
-    img: 'https://storage.cloud.google.com/handpicked/pilotjacket.png',
-    title: "Vintage Pilot Jacket",
-    price: "1299kr",
-    desc: "Featuring a classic pilot jacket design, this jacket has a timeless appeal that will never go out of style. It has a durable leather exterior and a warm, insulated interior, making it perfect for keeping warm on cold flights or on the ground. The jacket also has multiple pockets for storing small items, and a comfortable and adjustable fit.",
-    number: 1
-}
-const handpickedVest = {
-    img: 'https://storage.cloud.google.com/handpicked/blackvest.png',
-    title: "Our Legacy - Vest",
-    price: "749kr",
-    desc: "The Our Legacy Black Vest is a versatile and stylish piece of clothing that is perfect for any occasion. Made from high-quality materials, this vest is comfortable to wear and will last for years to come.",
-    number: 2
-}
-const handpickedHat = {
-    img: 'https://storage.cloud.google.com/handpicked/bluehat.png',
-    title: "COS - Hat",
-    price: "149kr",
-    desc: "The COS Blue Knit Beanie is a stylish and comfortable accessory that is perfect for keeping warm during the colder months. Made from high-quality knit material, this beanie is soft to the touch and will provide plenty of warmth and insulation on chilly days.",
-    number: 3
-}
+import React, { useState, useEffect } from "react";
+import jacket from "../assets/pilotjacket.png";
+import bluehat from "../assets/bluehat.png";
+import HandpickedCard from "./HandpickedCard";
+import Datafetching from "./Datafetching";
 
 export default function Handpicked() {
-    return (
-        <section className='handpicked-section'>
-            <h1 id="handpicked-title"><span id="hand">Hand</span> <span id="picked">Picked</span></h1>
-            <Datafetching />
-            <HandpickedCard {...0}/>
-            {/* <HandpickedCard {...handpickedVest} />
-            <HandpickedCard {...handpickedHat} /> */}
-            <button id="handpicked-seemore">See more!</button>
-        </section>
-    )
+  return (
+    <section className="handpicked-section">
+      <h1 id="handpicked-title">
+        <span id="hand">Hand</span> <span id="picked">Picked</span>
+      </h1>
+      <Datafetching />
+      <HandpickedCard />
+      <button id="handpicked-seemore">See more!</button>
+    </section>
+  );
 }
 window.addEventListener("scroll", () => {
-    const picked = document.getElementById("picked");
-    const jacket = document.getElementById("pilotjacket-img");
-    if (scrollY) {
-        picked.style.opacity = 0 + scrollY / 800;
-        picked.style.opacity > 0.6 ? document.getElementById("hand").style.opacity = 1 : console.log();
-    }
-})
+  const picked = document.getElementById("picked");
+  const jacket = document.getElementById("pilotjacket-img");
+  if (scrollY) {
+    picked.style.opacity = 0 + scrollY / 800;
+    picked.style.opacity > 0.6
+      ? (document.getElementById("hand").style.opacity = 1)
+      : console.log();
+  }
+});
--- Myrorna-vintage/src/intro-page/HandpickedCard.jsx
+++ Myrorna-vintage/src/intro-page/HandpickedCard.jsx
@@ -1,30 +1,57 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-export default  function HandpickedCard(index) {
-    const [products, setProducts] = useState([]);
-      useEffect(() => {
-     axios
-    .get("http://localhost:4000/products")
-    .then((res) => {
-      setProducts(res.data)
-     
-    })
-    .catch(console.error("err"));
-},[]);
+export default function HandpickedCard(props) {
+  const [products, setProducts] = useState([]);
+  let handpickedproducts = [];
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/products/handpicked")
+      .then((res) => {
+        console.log(res.data);
+        setProducts(res.data);
+        console.log(products);
+      })
+      .then(() => {})
+      .catch((error) => console.log(error));
+  }, []);
 
-const handpickedproducts = products.filter((product)=>product.handpicked)
-for(let i=0;i<handpickedproducts.length;i++){
-    console.log(handpickedproducts[i]);
-    return (
-        <section>
-            {
-           <h1>{handpickedproducts[i]}</h1>
-           }
+  return (
+    // .className?
+    <section className="handpicked-section">
+      {products.map((product, index) => {
+        if (index % 2 === 0) {
+          return (
+            <div
+              className="handpicked-card-container"
+              id="handpicked-right"
+              key={product.id}
+            >
+              <img src={"https://" + product.imgUrl} alt={product.type} />
+              <div className="handpicked-content">
+                <h1 className="handpicked-title">
+                  {product.brandName} {product.type}
+                </h1>
+                <h2>Price: {product.price * 10} kr</h2>
+                <h2>Size: {product.size}</h2>
+              </div>
+            </div>
+          );
+        } else {
+          return (
+            <div className="handpicked-card-container" key={product.id}>
+              <div className="handpicked-content">
+                <h1 className="handpicked-title">
+                  {product.brandName} {product.type}
+                </h1>
+                <h2>Price: {product.price * 10} kr</h2>
+                <h2>Size: {product.size}</h2>
+              </div>
+              <img src={"https://" + product.imgUrl} alt={product.type} />
+            </div>
+          );
+        }
+      })}
     </section>
-    )   
-
+  );
 }
-    
-}
-
